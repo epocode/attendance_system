809aefc4a971a903aa288c35fa464621b2104e5c
epoc <test@test.com>
2024-11-23 11:52:57 +0800

    update

--- ui/test.py
+++ ui/test.py
@@ -27,36 +27,25 @@ from PySide6.QtWidgets import (
     QVBoxLayout,
     QListWidget,
 )
-from PySide6.QtGui import QAction, QPixmap
+from PySide6.QtGui import QAction, QPixmap, QPalette, QColor
 from PySide6.QtCore import Qt, QSize
 
 
+class Color(QWidget):
+    def __init__(self, color):
+        super().__init__()
+        self.setAutoFillBackground(True)
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
+        
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('My App')
-        slider = QSlider(Qt.Orientation.Horizontal)
-        slider.setMinimum(-3)
-        slider.setMaximum(10)
-        slider.setSingleStep(3)  
-        slider.valueChanged.connect(self.value_chagned)
-        slider.sliderMoved.connect(self.slider_position)
-        slider.sliderPressed.connect(self.slider_pressed)
-        slider.sliderReleased.connect(self.slider_released)
-        self.setCentralWidget(slider)
-
-    def value_chagned(self, value):
-        print('value changed ', value)
-
-    def slider_position(self, position):
-        print('position: ', position)
-    
-    def slider_pressed(self):
-        print('pressed')
-    
-    def slider_released(self):
-        print('released')
+        widget = Color('red')
+        self.setCentralWidget(widget)
 
 
 os.chdir(pathlib.Path(__file__).parent)
